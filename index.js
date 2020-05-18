9e0d176d0c3fdcb8fab909c9d8fe4fefa24d16aa
klavs <klavs@septima.dk>
2020-05-18 10:49:40 +0200

    fixed types in index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import ClassRegistry from "@septima/septima-search/src/js/ClassRegistry"
 
 const demoTypes = new ClassRegistry("s3-demomodules/")
 //This is optional
-//In large package the types array may be generated from JsDoc
-demoTypes.setTypes([{
+//In large package the types object may be generated from JsDoc
+demoTypes.setTypes({
   ItemsForAddressProvider: {
     description: "Shows some demo details for a Dawa Adress"
   }
-}])
+})
 
 demoTypes.addClass(ItemsForAddressProvider, "ItemsForAddressProvider")
 
